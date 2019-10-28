5b39e8dffe6ec39a954b6ef5322095017a175c42
Mzwakali <thandomzwakali@gmail.com>
2019-10-28 09:43:44 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-5o7jqg
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-5o7jqg)
---
+++ src/app/employee.ts
@@ -0,0 +1,8 @@
+export class Employee {
+  id: string;
+  firstName: string;
+  lastName: string;
+  idNum: string;
+  jobTitle: string;
+  active: boolean;
+}
